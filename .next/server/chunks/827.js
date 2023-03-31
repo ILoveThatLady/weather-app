f89b499d08052f4205d25f4253728b4752a314ce
Emilio Ortiz <ortizemilio.pers@gmail.com>
2023-03-31 14:54:27 -0600

    stable

---
+++ .next/server/chunks/827.js
@@ -0,0 +1,63 @@
+exports.id = 827;
+exports.ids = [827];
+exports.modules = {
+
+/***/ 914:
+/***/ ((module) => {
+
+// Exports
+module.exports = {
+	"date": "GetDate_date__IeCY8",
+	"slide-in-right": "GetDate_slide-in-right___02Pd"
+};
+
+
+/***/ }),
+
+/***/ 827:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(689);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _components_getDate_GetDate_module_css__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(914);
+/* harmony import */ var _components_getDate_GetDate_module_css__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_components_getDate_GetDate_module_css__WEBPACK_IMPORTED_MODULE_2__);
+
+
+
+const GetDate = ()=>{
+    const [currentDate, setCurrentDate] = (0,react__WEBPACK_IMPORTED_MODULE_1__.useState)("");
+    (0,react__WEBPACK_IMPORTED_MODULE_1__.useEffect)(()=>{
+        const dateOptions = {
+            month: "long",
+            day: "numeric"
+        };
+        const dayOptions = {
+            weekday: "long"
+        };
+        const currentDate = new Date();
+        const formattedDate = `
+        ${currentDate.toLocaleDateString("en-US", dateOptions)} -
+        ${currentDate.toLocaleDateString("en-US", dayOptions)}
+        `;
+        setCurrentDate(formattedDate);
+    }, []);
+    return /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+        className: (_components_getDate_GetDate_module_css__WEBPACK_IMPORTED_MODULE_2___default().date),
+        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("p", {
+            children: currentDate
+        })
+    });
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (GetDate);
+
+
+/***/ })
+
+};
+;
---
+++ .next/server/pages/api/weather.js
@@ -0,0 +1,34 @@
+"use strict";
+(() => {
+var exports = {};
+exports.id = 743;
+exports.ids = [743];
+exports.modules = {
+
+/***/ 431:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ fetchWeather)
+/* harmony export */ });
+async function fetchWeather(query) {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${"f8fe6389d7486cf3724e283f6fb7deaa"}&units=metric`);
+    const data = await response.json();
+    return data;
+}
+
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../../webpack-api-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__(431));
+module.exports = __webpack_exports__;
+
+})();
